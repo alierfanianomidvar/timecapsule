bdcfc756b9ba28edf5712ecdacf9ee52dea5d86d
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2023-12-01 17:18:38 +0100

    Add: the http call to the external api base on the url.
    History fact of the day.

---
+++ src/main/java/com/today/timecapsule/business/service/HttpConnectionService.java
@@ -0,0 +1,10 @@
+package com.today.timecapsule.business.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public interface HttpConnectionService {
+
+    JsonNode get(
+            String url
+    );
+}
---
+++ src/main/java/com/today/timecapsule/business/service/Imp/HttpConnectionServiceImp.java
@@ -0,0 +1,33 @@
+package com.today.timecapsule.business.service.Imp;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.today.timecapsule.business.service.HttpConnectionService;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class HttpConnectionServiceImp implements HttpConnectionService {
+    @Override
+    public JsonNode get(String url) {
+
+
+        RestTemplate restTemplate = new RestTemplate();
+        //ResponseEntity<JsonNode> response = restTemplate.getForEntity(url, JsonNode.class);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("X-Api-Key", "IBfNeqbpdJKd0FnLqhdu0g==gA6HvF9MTHv6uT7V");
+        httpHeaders.set("accept", "application/json");
+        HttpEntity<String> entity = new HttpEntity<>(httpHeaders);
+
+        ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.GET, entity, JsonNode.class);
+
+        JsonNode root = response.getBody();
+        return root;
+    }
+}
