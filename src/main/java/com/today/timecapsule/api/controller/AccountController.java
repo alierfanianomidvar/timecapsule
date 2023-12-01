d0b07de38c4fb23186c21ece4c0822f9d1cd8892
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2023-12-01 14:37:21 +0100

    Initial commit
    api structure

---
+++ src/main/java/com/today/timecapsule/api/controller/AccountController.java
@@ -0,0 +1,12 @@
+package com.today.timecapsule.api.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping(value = "/account")
+public class AccountController {
+
+
+}
